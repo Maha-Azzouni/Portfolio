594499fda312aa55156e59e63f503e7b33f1c35d
Maha <maha.k.azzouni@gmail.com>
2026-08-23 11:13:15 +0300

    feat: add skills section

---
+++ src/components/Skills.jsx
@@ -0,0 +1,30 @@
+import "../styles/Skills.css";
+
+function Skills() {
+  return (
+    <section className="skills" id="skills">
+      <div className="skills-list">
+        <div className="skill">01 — HTML</div>
+        <div className="skill">02 — CSS</div>
+        <div className="skill">03 — JavaScript</div>
+        <div className="skill">04 — C#</div>
+        <div className="skill">05 — ASP.net</div>
+        <div className="skill">06 — Paython</div>
+        <div className="skill">07 — React</div>
+        <div className="skill">08 — n8n</div>
+        <div className="skill">09 — Git & GitHub</div>
+      </div>
+      <div className="skills-title">
+        <p>MY SKILLS</p>
+
+        <h2>
+          WHAT I
+          <br />
+          WORK WITH
+        </h2>
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
