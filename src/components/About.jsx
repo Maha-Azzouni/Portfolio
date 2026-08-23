13fed238918c064480b8b70dea980628e098ae9c
Maha <maha.k.azzouni@gmail.com>
2026-08-23 11:12:25 +0300

    feat: add about section

---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import "../styles/About.css";
+
+function About() {
+  return (
+    <section className="about" id="about">
+      <div className="about-title">
+        <p>ABOUT ME</p>
+        <h3>
+          A LITTLE <br />
+          ABOUT ME
+        </h3>
+      </div>
+      <div className="about-content">
+        <p>
+          I'm a Computer Systems Engineering student and Full-Stack Developer
+          who enjoys building modern, responsive, and user-friendly websites.
+        </p>
+
+        <p>
+          I work with React, JavaScript, ASP.net, HTML, CSS, and Tailwind CSS,
+          and I'm always learning new technologies and improving my skills.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
